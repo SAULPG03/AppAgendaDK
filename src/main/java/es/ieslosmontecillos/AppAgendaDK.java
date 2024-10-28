2fea91ae0ddeb65257fcfb6b7300f670c77f8cfa
Saúl Portales García <sporgar2706@g.educaand.es>
2024-10-28 14:55:45 +0100

    Falta terminar el inicio del frontend

--- src/main/java/es/ieslosmontecillos/AppAgendaDK.java
+++ src/main/java/es/ieslosmontecillos/AppAgendaDK.java
@@ -1,44 +1,52 @@
 package es.ieslosmontecillos;
-
-import es.ieslosmontecillos.views.PrimaryView;
-import es.ieslosmontecillos.views.SecondaryView;
-import com.gluonhq.charm.glisten.application.AppManager;
-import com.gluonhq.charm.glisten.visual.Swatch;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
-
+import com.gluonhq.charm.glisten.mvc.View;
+import java.io.IOException;
+/**
+ * @author profesora
+ */
 public class AppAgendaDK extends Application {
 
-    public static final String PRIMARY_VIEW = HOME_VIEW;
-    public static final String SECONDARY_VIEW = "Secondary View";
-
-    private final AppManager appManager = AppManager.initialize(this::postInit);
-
-    @Override
-    public void init() {
-        appManager.addViewFactory(PRIMARY_VIEW, () -> new PrimaryView().getView());
-        appManager.addViewFactory(SECONDARY_VIEW, () -> new SecondaryView().getView());
-
-        DrawerManager.buildDrawer(appManager);
-    }
+    private DataUtil dataUtil;
+    private InicioView inicioView = new InicioView();
+    private InicioController inicioController;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        appManager.start(primaryStage);
+    public void start(Stage primaryStage) throws IOException {
+        StackPane rootMain = new StackPane();
+        View inicio = inicioView.getView();
+        rootMain.getChildren().add(inicio);
+        inicioController = inicioView.getInicioController();
+        inicioController.setRootMain(rootMain);
+        // Solicitamos los datos a los servicios de dataUtil
+        dataUtil = new DataUtil();
+        dataUtil.obtenerTodasProvincias();
+        ObservableList<Provincia> olProv = dataUtil.getOlProvincias();
+        dataUtil.obtenerTodasPersonas();
+        ObservableList<Persona> olPers = dataUtil.getOlPersonas();
+        // Pasamos los datos obtenidos a la clase controladora de inicio
+        inicioController.setDataUtil(dataUtil);
+        inicioController.setOlProv(olProv);
+        inicioController.setOlPers(olPers);
+        inicioController.setRootMain(rootMain);
+        Scene scene = new Scene(rootMain,758,482);
+        primaryStage.setTitle("App Agenda");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
-    private void postInit(Scene scene) {
-        Swatch.BLUE.assignTo(scene);
-
-        scene.getStylesheets().add(AppAgendaDK.class.getResource("style.css").toExternalForm());
-        ((Stage) scene.getWindow()).getIcons().add(new Image(AppAgendaDK.class.getResourceAsStream("/icon.png")));
+    @Override
+    public void stop() {
+        System.out.println("Stop: Se cerró la app");
     }
-
-    public static void main(String args[]) {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
         launch(args);
     }
 }
